17ea0cd5e9dc25c501cb3c5a957e58ab29ff432e
oriske <oliver.riske@nextag.ch>
2016-05-14 20:47:09 +0200

    Initial commit

---
+++ CreateExcel/src/ch/next/sigvaris/model/Artikel.java
@@ -0,0 +1,14 @@
+package ch.next.sigvaris.model;
+
+import java.util.Hashtable;
+
+public class Artikel {
+
+	public String Key;
+	
+	public Hashtable<String,String> stringObjects = new Hashtable<String,String>();
+	public Hashtable<String,Integer> integerObjects = new Hashtable<String,Integer>();
+	public Hashtable<String,Double> doubleObjects = new Hashtable<String,Double>();
+			
+	
+}
